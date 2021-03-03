3cf10fca76e732d18e5c4a7d8857e3912fa7e337
mcine <mcine74@hotmail.com>
2021-03-03 21:36:41 +0200

    first working version

--- buttonbox/buttonbox.ino
+++ buttonbox/buttonbox.ino
@@ -1,3 +1,6 @@
+#include <Joystick.h>
+
+Joystick_ Joystick(0x06, JOYSTICK_TYPE_JOYSTICK, 54, 0, false, false, false, false, false, false, false, false, false, false, false);
 
 struct input
 {
@@ -18,7 +21,7 @@ encoder enc2 = {10, 16, 0,0,0};
 encoder enc1 = {14, 15, 0,0,0};
 
 int RXLED = 17;
-const int numInputs = 14;
+const int numInputs = 18;
 input inputStates[numInputs];
 
 void setup() {
@@ -34,20 +37,24 @@ void setup() {
   pinMode(1, OUTPUT);
   pinMode(2, OUTPUT);
 
-  for(int i = 3; i<21; i=i+1)
+  for(int i = 3; i<20; i=i+1)
     pinMode(i, INPUT);
-  for(int i = 0; i<18; i=i+1)
+  pinMode(20, INPUT_PULLUP);
+  pinMode(21, INPUT_PULLUP);
+  for(int i = 0; i<numInputs; i=i+1)
     inputStates[i] = {0,0};
     
   digitalWrite(0, HIGH);
   digitalWrite(1, LOW);
   digitalWrite(2, HIGH); // encoder power
   //pinMode(RXLED, OUTPUT);  // Set RX LED as an output
+  Joystick.begin();
 }
 
 void loop() 
 {
   static int modeSwitch = 0;
+  bool usedelay = false;
   for(int r = 0; r<2 ; r=r+1)
   {
     SetRow(r);
@@ -57,9 +64,13 @@ void loop()
     }
   }
 
-  inputStates[12].state = digitalRead(20);
-  inputStates[13].state = digitalRead(21);
-
+  //encoder buttons
+  inputStates[12].state = !digitalRead(20);
+  inputStates[13].state = !digitalRead(21);
+  if((inputStates[12].state == 0 && inputStates[12].previousState == 1) ||
+      (inputStates[13].state == 0 && inputStates[13].previousState == 1))
+    usedelay = true;
+      
   int sw = digitalRead(18) + (digitalRead(19)<<1);
   if(sw != modeSwitch) 
   { 
@@ -67,7 +78,47 @@ void loop()
     modeSwitch = sw;
   }
 
-  for(int i = 0; i<numInputs-2 ; i=i+1)
+
+
+  int e1State = getEncoderState(enc1);
+  switch(e1State) 
+  {
+    case 1:
+      inputStates[14].state = 1;
+      usedelay = true;
+      break;
+
+    case -1:
+      inputStates[15].state = 1;
+      usedelay = true;
+      break;
+
+    default:
+      inputStates[14].state = 0;
+      inputStates[15].state = 0;
+      break;
+  }
+
+  int e2State = getEncoderState(enc2);
+  switch(e2State) 
+  {
+    case 1:
+      inputStates[16].state = 1;
+      usedelay = true;
+      break;
+
+    case -1:
+    usedelay = true;
+      inputStates[17].state = 1;
+      break;
+
+    default:
+      inputStates[16].state = 0;
+      inputStates[17].state = 0;
+      break;
+  }
+
+/*  for(int i = 0; i<numInputs ; i=i+1)
   {
     if(inputStates[i].previousState != inputStates[i].state)
     {
@@ -79,25 +130,31 @@ void loop()
         Serial.println(" OFF.");
     }
   }
-
-  int e1State = getEncoderState(enc1);
-  if(e1State!=0) 
+  */
+  // set joystick buttons:
+  for(int c = 0; c<numInputs ; c=c+1)
   {
-    Serial.print("ENC1: "); Serial.println(e1State);
+    if(inputStates[c].previousState != inputStates[c].state)
+    {
+      Joystick.setButton(modeSwitch*numInputs + c, inputStates[c].state);
+      Serial.print("Joystick button.");
+      Serial.print(modeSwitch*numInputs + c+1);
+      if(inputStates[c].state>0)
+        Serial.println(" PRESSED.");
+      else
+        Serial.println(" RELEASED.");
+    }
   }
 
-  int e2State = getEncoderState(enc2);
-  if(e2State!=0) 
-  {
-    Serial.print("ENC2: "); Serial.println(e2State);
-  }
-
+  // reset states
   for(int c = 0; c<numInputs ; c=c+1)
     inputStates[c].previousState = inputStates[c].state;
   enc1.previousState = enc1.state;
   enc2.previousState = enc2.state;
   inputStates[12].previousState = inputStates[12].state;
   inputStates[13].previousState = inputStates[13].state;
+
+  if(usedelay) delay(50);
 }
 
 void SetRow(int row)
