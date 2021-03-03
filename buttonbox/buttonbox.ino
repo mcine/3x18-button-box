8daa5f813c4970764a77abb0b4deedd0271deb33
mcine <mcine74@hotmail.com>
2021-03-03 22:45:57 +0200

    delayt tehty fiksummin

--- buttonbox/buttonbox.ino
+++ buttonbox/buttonbox.ino
@@ -6,6 +6,7 @@ struct input
 {
   int state;
   int previousState;
+  unsigned long lastReleased;
 };
 
 struct encoder
@@ -15,7 +16,12 @@ struct encoder
   int previousClkState;
   int state;
   int previousState;
+  unsigned long lastChange;
 };
+
+const int DISABLED_TIME = 100;
+
+const int ENCODER_TIME = 50;
 
 encoder enc2 = {10, 16, 0,0,0};
 encoder enc1 = {14, 15, 0,0,0};
@@ -42,7 +48,7 @@ void setup() {
   pinMode(20, INPUT_PULLUP);
   pinMode(21, INPUT_PULLUP);
   for(int i = 0; i<numInputs; i=i+1)
-    inputStates[i] = {0,0};
+    inputStates[i] = {0,0,0};
     
   digitalWrite(0, HIGH);
   digitalWrite(1, LOW);
@@ -54,22 +60,36 @@ void setup() {
 void loop() 
 {
   static int modeSwitch = 0;
-  bool usedelay = false;
+  unsigned long currentTime = millis();
+  //bool usedelay = false;
   for(int r = 0; r<2 ; r=r+1)
   {
     SetRow(r);
     for(int c = 0; c<6 ; c=c+1)
     {
-      inputStates[r*6+c].state = digitalRead(c+3);
+      int currentInput = r*6+c;
+      if(inputActive(currentInput, currentTime))
+      {
+        inputStates[currentInput].state = digitalRead(c+3);
+        updateReleased(currentInput, currentTime);
+      }
     }
   }
 
   //encoder buttons
-  inputStates[12].state = !digitalRead(20);
-  inputStates[13].state = !digitalRead(21);
-  if((inputStates[12].state == 0 && inputStates[12].previousState == 1) ||
-      (inputStates[13].state == 0 && inputStates[13].previousState == 1))
-    usedelay = true;
+  if(inputActive(12, currentTime))
+  {
+    inputStates[12].state = !digitalRead(20);
+    updateReleased(12, currentTime);
+  }
+  if(inputActive(13, currentTime))
+  {
+    inputStates[13].state = !digitalRead(21);
+    updateReleased(13, currentTime);
+  }
+  //if((inputStates[12].state == 0 && inputStates[12].previousState == 1) ||
+  //    (inputStates[13].state == 0 && inputStates[13].previousState == 1))
+  //  usedelay = true;
       
   int sw = digitalRead(18) + (digitalRead(19)<<1);
   if(sw != modeSwitch) 
@@ -84,18 +104,22 @@ void loop()
   switch(e1State) 
   {
     case 1:
-      inputStates[14].state = 1;
-      usedelay = true;
+      if(inputActive(14, currentTime))
+        inputStates[14].state = 1;
+      //usedelay = true;
       break;
 
     case -1:
-      inputStates[15].state = 1;
-      usedelay = true;
+      if(inputActive(15, currentTime))
+        inputStates[15].state = 1;
+      //usedelay = true;
       break;
 
     default:
       inputStates[14].state = 0;
+      updateReleased(14, currentTime);
       inputStates[15].state = 0;
+      updateReleased(15, currentTime);
       break;
   }
 
@@ -103,18 +127,20 @@ void loop()
   switch(e2State) 
   {
     case 1:
-      inputStates[16].state = 1;
-      usedelay = true;
+      if(inputActive(16, currentTime))
+        inputStates[16].state = 1;
       break;
 
     case -1:
-    usedelay = true;
-      inputStates[17].state = 1;
+      if(inputActive(17, currentTime))
+        inputStates[17].state = 1;
       break;
 
     default:
       inputStates[16].state = 0;
+      updateReleased(16, currentTime);
       inputStates[17].state = 0;
+      updateReleased(17, currentTime);
       break;
   }
 
@@ -154,9 +180,20 @@ void loop()
   inputStates[12].previousState = inputStates[12].state;
   inputStates[13].previousState = inputStates[13].state;
 
-  if(usedelay) delay(50);
+  //if(usedelay) delay(50);
 }
 
+bool inputActive(int index,  unsigned long currentTime)
+{
+  return currentTime > DISABLED_TIME + inputStates[index].lastReleased;
+}
+
+void updateReleased(int index, unsigned long currentTime)
+{
+  if(inputStates[index].state == 0 && inputStates[index].previousState == 1)
+          inputStates[index].lastReleased = currentTime;
+}
+        
 void SetRow(int row)
 {
   switch (row)
@@ -175,22 +212,25 @@ void SetRow(int row)
 }
 int getEncoderState (encoder &enc)
 {
+  int ret = enc.state;
   int clk = digitalRead(enc.clkPin);
-  int ret = enc.state = 0;
-  if( clk != enc.previousClkState)
+  if(millis()>ENCODER_TIME + enc.lastChange)
   {
-    int dt = digitalRead(enc.dtPin);
-    if( dt == clk)
+    ret = enc.state = 0;
+    if( clk != enc.previousClkState)
     {
-      enc.state = ret = 1;
+      int dt = digitalRead(enc.dtPin);
+      if( dt == clk)
+      {
+        enc.state = ret = 1;
+      }
+      else
+      {
+        enc.state = ret = -1;
+      }
+      enc.lastChange = millis();   
     }
-    else
-    {
-      enc.state = ret = -1;
-    }        
   }
-
   enc.previousClkState = clk;
-
   return ret;
 }
